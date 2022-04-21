fe3742f23cc991ce8f28146255eaa51cb1fb4e76
batzuu <utkarsh1616@gmail.com>
2022-04-22 01:51:53 +0530

    some testing code.. will be removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
+function B(newProp) {
+	newProp.bye = "no"
+	return (
+		<div>
+			<h1>YE B WAALA HAI</h1>
+			<A name={newProp.name} />
+		</div>
+	)
+}
+
+function A(prop) {
+	return (
+		<div>
+			<h1>This is the name property {prop.name}</h1>
+			<h1>This is the class property {prop.class}</h1>
+		</div>
+	)
+}
+
 function App() {
 	return (
-		<div className="App">
-			
+		<div>
+			<B name="Tyagi ji" class="elite" />
+			<B name="Tyagi ji bhai" class="elite" />
+			<B name="Tyagi ji dost" class="elite" />
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-let test = <h1>BYe BYe</h1>
-root.render(test);
+
+let testEle = <div></div>
+root.render(testEle)
+
+function doSecBad() {
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
+
+setInterval(doSecBad, 2000);
