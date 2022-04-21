971e3630f57484c1572c4ca3fd76b6f6c8c24c45
batzuu <utkarsh1616@gmail.com>
2022-04-22 02:23:12 +0530

    nav css added and some bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import NavBar from "./navbar/NavBar";
 
 export default function App() {
 	return (
 		<>
-			
+			<NavBar />
 		</>
 	);
 }
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,9 @@
+import './NavBar.css'
+
+let NavBar = () => {
+	return (
+		<div className="top">NavBar Icon</div>
+	)
+}
+
+export default NavBar;
