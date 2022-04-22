770a36d56df9f1dec9f4d3d578759967d5e5e147
sutap2000 <sutappandey@gmail.com>
2022-04-23 00:09:44 +0530

    Added icons to NavBar, Listed HOME ABOUT WRITE AND LOGIN with Blog photo

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,8 +1,26 @@
-import './NavBar.css'
-
+import './NavBar.css';
+import pic from '../assets/tss.jpg';
 export default () => {
 	return (
-		<div className="top">NavBar Icon</div>
-		
+		<div className="top">
+			<div className="topLeft">
+				<i className="topIcon fa-brands fa-facebook-square"></i>
+				<i className="topIcon fa-brands fa-twitter-square"></i>
+				<i className="topIcon fa-brands fa-linkedin"></i>
+				<i className="topIcon fa-brands fa-instagram-square"></i>
+			</div>
+			<div className="topCenter">
+				<ul className="topList">
+					<li className="topListItem">HOME</li>
+					<li className="topListItem">ABOUT</li>
+					<li className="topListItem">WRITE</li>
+					<li className="topListItem">LOGOUT</li>
+				</ul>
+			</div>
+			<div className="topRight">
+				<i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+				<img className="topImg" src={pic} alt="img not found" />
+			</div>
+		</div>
 	)
 }
