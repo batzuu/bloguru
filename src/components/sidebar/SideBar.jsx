8c5a4e1c1cb9e71fcbe40b0c41fb04469d14ac36
batzuu <utkarsh1616@gmail.com>
2022-04-24 01:55:22 +0530

    read SideBar.jsx comment

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
 import './sidebar.css'
 export default function SideBar() {
-  return (
-	<div>
-	  sidebar
-	</div>
-  )
+	// Note from batzu: 
+	// dont use rfc snippet as it inserts two spaces intead of a tab or
+	// modify the snippet to contain tabs(/t) insteat of space
+	// also all lowercase css names sidebar.css not SideBar.css
+	// B))
+	return (
+		<div>
+			sidebar
+		</div>
+	)
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import NavBar from "../../components/navbar/NavBar"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import SideBar from "../../components/sidebar/SideBar"
