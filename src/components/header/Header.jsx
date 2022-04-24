220477bcaa2fd605e4d5499eb701f3fe7d47e80c
sutap2000 <sutappandey@gmail.com>
2022-04-25 00:20:12 +0530

    Tried some more fonts and corrected classname in header file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
 	return (
 		<div className="header">
 			<div className="headerTitles">
+				<span className="headerTitlesSm">A TSTS Production</span>
 				<span className="headerTitlesLg">Bloguru</span>
-				<span className="headTitlesSm">smol </span>
 			</div>
 			<img className="headerImg" src="https://i.imgur.com/f1NJjkE.jpeg" alt="google se image" />
 		</div>	
