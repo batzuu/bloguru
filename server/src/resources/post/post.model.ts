34b7c88aeef21de870e764e001fbbb2ddbd3fcee
batzuu <utkarsh1616@gmail.com>
2022-05-07 21:39:18 +0530

    minor formatting change

--- server/src/resources/post/post.model.ts
+++ server/src/resources/post/post.model.ts
@@ -1,19 +1,22 @@
-import { model, Schema } from "mongoose";
-import Post from "@/resources/post/post.interface";
+import { model, Schema } from 'mongoose';
+import Post from '@/resources/post/post.interface';
 
-const postSchema = new Schema({
-	title: {
-		type: String,
-		required: true,
+const postSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		body: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: String,
+			required: true,
+		},
 	},
-	body: {
-		type: String,
-		required: true,
-	},
-	author: {
-		type: String,
-		required: true
-	}
-}, { timestamps: true });
+	{ timestamps: true }
+);
 
 export default model<Post>('Post', postSchema);
