3039e10d47b6e08b44e7bd8e0d9fabcf43ad462d
sutap2000 <sutappandey@gmail.com>
2022-05-31 01:15:14 +0530

    Added login, register, write and settings pages. No functionality linked yet

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import Home from "./pages/home/Home"
 import NavBar from "./components/navbar/NavBar"
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
 } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 export default function App() {
 	return (
@@ -17,6 +20,9 @@ export default function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/post" element={<Single />} />
 					<Route path="/write" element={<Write />} />
+					<Route path="/usersettings" element={<Settings />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
 				</Routes>
 			</Router>
 		</>
