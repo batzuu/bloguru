2e9e592a4f0744e9f3515159e8da3b125852ba4a
yugal1005 <yugalpanchal01@gmail.com>
2022-09-18 12:29:42 +0530

    completed login screen and started home screen

--- app/lib/screens/login_screen.dart
+++ app/lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/user_input_login.dart';
+import './home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,38 +13,112 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(13, 13, 13, 1),
-      body: Column(
-        children: [
-          // const SizedBox(
-          //   height: 180,
-          // ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 180,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 180,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: "Blo",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                  children: const [
+                    TextSpan(
+                      text: "guru",
+                      style: TextStyle(
+                        color: Color.fromRGBO(181, 111, 241, 1),
+                        fontFamily: "Poppins",
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: RichText(
+            const SizedBox(
+              height: 30,
+            ),
+            const UserInputLogin(),
+            const SizedBox(
+              height: 10,
+            ),
+            RichText(
               text: TextSpan(
-                text: "Blo",
-                style: Theme.of(context).textTheme.headlineLarge,
-                children: const [
+                text: "Forget Password? ",
+                style: Theme.of(context).textTheme.bodyText1,
+                children: [
                   TextSpan(
-                    text: "guru",
-                    style: TextStyle(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        debugPrint("forget password");
+                      },
+                    text: "Recover here",
+                    style: const TextStyle(
                       color: Color.fromRGBO(181, 111, 241, 1),
                       fontFamily: "Poppins",
-                      fontSize: 48,
+                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const UserInputLogin(),
-        ],
+            const SizedBox(
+              height: 80,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Divider(
+                endIndent: 30,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, HomeScreen.routeName);
+                    },
+                    child: const Text(
+                      "Skip for now",
+                      style: TextStyle(
+                        color: Color.fromRGBO(181, 111, 241, 1),
+                        fontFamily: "Poppins",
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: "New User? ",
+                      style: Theme.of(context).textTheme.bodyText1,
+                      children: [
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                          text: "SignUp",
+                          style: const TextStyle(
+                            color: Color.fromRGBO(181, 111, 241, 1),
+                            fontFamily: "Poppins",
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- app/lib/widgets/user_input_login.dart
+++ app/lib/widgets/user_input_login.dart
@@ -8,50 +8,125 @@ class UserInputLogin extends StatefulWidget {
 }
 
 class _UserInputLoginState extends State<UserInputLogin> {
-  bool _isValidEmail(String email) {
-    return RegExp(
-            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-        .hasMatch(email);
+  bool autoLogin = false;
+
+  UnderlineInputBorder buildInputBorderRadius() {
+    return UnderlineInputBorder(
+      borderSide: const BorderSide(color: Color.fromRGBO(80, 80, 80, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Form(
-        child: Column(
-          children: [
-            Container(
-              // height: 65,
-              margin: const EdgeInsets.only(left: 15, right: 15),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: const Color.fromRGBO(80, 80, 80, 1)),
-              child: TextFormField(
-                style: Theme.of(context).textTheme.bodyText1,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  // focusColor: Colors.white,
-                  alignLabelWithHint: true,
-                  prefixIcon: const Icon(
-                    Icons.email_outlined,
-                    color: Colors.white,
-                  ),
-                  // prefixIconColor: Colors.white,
-                  hintText: "Email",
-                  hintStyle: Theme.of(context).textTheme.bodyText1,
-                  border: InputBorder.none,
+    return Form(
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.all(10),
+            child: TextFormField(
+              style: Theme.of(context).textTheme.bodyText1,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromRGBO(80, 80, 80, 1),
+                focusedErrorBorder: buildInputBorderRadius(),
+                enabledBorder: buildInputBorderRadius(),
+                focusedBorder: buildInputBorderRadius(),
+                errorBorder: buildInputBorderRadius(),
+                prefixIcon: const Icon(
+                  Icons.email_outlined,
+                  color: Colors.white,
                 ),
-                autovalidateMode: AutovalidateMode.always,
-                validator: (val) => _isValidEmail(val.toString())
-                    ? null
-                    : "Entred email is not valid",
+                hintText: "Email",
+                hintStyle: Theme.of(context).textTheme.bodyText1,
+                border: InputBorder.none,
               ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (val) => _isValidEmail(val.toString())
+                  ? null
+                  : "Entred email is not valid",
             ),
-          ],
-        ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.all(10),
+            child: TextFormField(
+              obscureText: true,
+              style: Theme.of(context).textTheme.bodyText1,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromRGBO(80, 80, 80, 1),
+                focusedErrorBorder: buildInputBorderRadius(),
+                enabledBorder: buildInputBorderRadius(),
+                focusedBorder: buildInputBorderRadius(),
+                errorBorder: buildInputBorderRadius(),
+                prefixIcon: const Icon(
+                  Icons.lock_outline_rounded,
+                  color: Colors.white,
+                ),
+                hintText: "Password",
+                hintStyle: Theme.of(context).textTheme.bodyText1,
+                border: InputBorder.none,
+              ),
+              // autovalidateMode: AutovalidateMode.always,
+              // validator: (val) => _isValidEmail(val.toString())
+              //     ? null
+              //     : "Entred email is not valid",
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Theme(
+                  data: ThemeData(unselectedWidgetColor: Colors.white),
+                  child: Checkbox(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    activeColor: Theme.of(context).primaryColor,
+                    checkColor: Colors.black,
+                    value: autoLogin,
+                    onChanged: (val) {
+                      setState(() {
+                        autoLogin = val ?? false;
+                      });
+                    },
+                  ),
+                ),
+                Text(
+                  "Keep me signed in",
+                  style: Theme.of(context).textTheme.bodyText1,
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all(const Size(300, 50)),
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).primaryColor),
+            ),
+            onPressed: () {},
+            child: Text(
+              "Login",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          )
+        ],
       ),
+      // ),
     );
+  }
+
+  bool _isValidEmail(String email) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(email);
   }
 }
